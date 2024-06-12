28047e1254d4384162c3268e8d47570ea5ae74aa
Krittika <krittikajari@gmail.com>
2024-06-12 21:16:51 +0530

    Weather API integrated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+const APIKey1 = "a9756d311f499a7d1892ce927bb4fde8";
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -12,8 +14,33 @@ app.get("/",(req,res)=>{
 });
 
 app.post("/get-weather", async (req,res)=>{
+try{
+    //getting city name from user
     const city = req.body.cityName;
-    let geoCodeResponse = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`);
+    //getting latitude and longitude from city name using geo coder api 
+    const geoCodeResponse = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city},IND&limit=1&appid=${APIKey1}`);
+    const geoCodeResult = geoCodeResponse.data;
+    console.log(geoCodeResult);
+    const latitude = geoCodeResult[0].lat;
+    const longitude = geoCodeResult[0].lon;
+    console.log(latitude,longitude);
+    //pass longitude and latitude to get weather details
+    const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey1}`)
+    const weatherResult = weatherResponse.data;
+    console.log(weatherResult);
+   
+    res.render("result.ejs",{
+        city : city,
+        temperature : weatherResult.main.temp,
+        maxTemperature: weatherResult.main.temp_max,
+        minTemperature: weatherResult.main.temp_min,
+        description: weatherResult. weather[0].description
+    });
+}
+catch(error){
+    res.render("result.ejs",{data: error.data});
+    console.log(error.data);
+}
 });
 
 app.listen(port,()=>{
